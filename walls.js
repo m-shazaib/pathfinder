4d2093f318b8f0cd19f49d6f2a757554d6f5016d
Muhammad Shahzaib <shahzaib3742@email.com>
2023-06-04 18:33:57 +0500

    made it more structured by shifting it to another file

---
+++ walls.js
@@ -0,0 +1,66 @@
+import Cell,{walls,cells} from './index.js';
+
+
+class WallCell extends Cell {
+    constructor(id) {
+      super(id);
+    }
+  
+    setColor(color) {
+      if (color === 'black') {
+        super.setColor(color);
+      } else {
+        // Prevent changing the color of wall cells to other colors
+        return;
+      }
+    }
+  }
+
+  
+ 
+  
+  let isLeftMouseDown = false;
+  let isMiddleMouseDown = false;
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    
+   for(let cell of cells){
+    document.addEventListener('mousedown', function(event) {
+        if (event.button === 0) {
+          isLeftMouseDown = true;
+        } else if (event.button === 1) {
+          isMiddleMouseDown = true;
+        }
+      });
+    
+      document.addEventListener('mouseup', function(event) {
+        if (event.button === 0) {
+          isLeftMouseDown = false;
+        } else if (event.button === 1) {
+          isMiddleMouseDown = false;
+        }
+      });
+    
+    
+    cell.addEventListener('mouseover', function(event) {
+        if (isLeftMouseDown) {
+          const hoveredCell = new WallCell(cell.id);
+          walls.push(hoveredCell);
+          hoveredCell.setColor('black');
+          // console.log(walls);
+        } else if (isMiddleMouseDown) {
+          const hoveredCell = new Cell(cell.id);
+          hoveredCell.setColor('white');
+          // remove the cell from walls array
+          const index=walls.findIndex(walls=>walls.id===cell.id)
+          // the code goes over here,
+          // console.log(walls[0]);
+
+        }
+    
+      });
+    }
+
+    });
+
+  
